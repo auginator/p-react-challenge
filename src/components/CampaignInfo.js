926aee89f60a213e71017c9b9eeb337fc322ff7f
Agustin Sevilla <amsevilla3@gmail.com>
2019-12-11 14:20:50 -0800

    Item 1 - placeholders for total contributions

--- src/components/CampaignInfo.js
+++ src/components/CampaignInfo.js
@@ -2,16 +2,16 @@ import './CampaignInfo.css'
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { getCampaignById, getSelectedCampaignId, getCampaignContributions } from '../modules'
+import { getCampaignById, getSelectedCampaignId, getCampaignContributions, getCampaignContributionsTotal} from '../modules'
 
 import DonateForm from './DonateForm'
 import Contributions from './CampaignContributions'
 import CampaignDetails from './CampaignDetails'
 
-function CampaignInfo({ campaign, contributions }) {
+function CampaignInfo({ campaign, contributions, campaignContributionsTotal }) {
   return (
     <div className="CampaignInfo">
-      <CampaignDetails campaign={ campaign } />
+      <CampaignDetails campaign={campaign} campaignContributionsTotal={campaignContributionsTotal}/>
       <Contributions contributions={ contributions } />
       <DonateForm campaign={ campaign } />
     </div>
@@ -22,8 +22,8 @@ const mapStateToProps = function(state) {
   const selectedCampaignId = getSelectedCampaignId(state)
   const campaign = getCampaignById(state, selectedCampaignId )
   const contributions = getCampaignContributions(state, selectedCampaignId)
-
-  return { campaign, contributions }
+  const campaignContributionsTotal = getCampaignContributionsTotal(state, selectedCampaignId)
+  return { campaign, contributions, campaignContributionsTotal }
 }
 
 const mapDispatchToProps = {
